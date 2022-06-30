9e729f6cabcb8247e9ddd6aca8ec73cb7ecbf53a
frontyoon <issou06261@gmail.com>
2022-06-30 10:20:41 +0900

    add host

--- ML/settings.py
+++ ML/settings.py
@@ -42,7 +42,9 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '10.120.72.237',
+]
 
 
 # Application definition
